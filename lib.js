46156e0db5904bde2802ad958dfa44b4bf04f063
fuguanghe <fgh_email@163.com>
2019-06-15 10:05:29 +0800

    首页字典

---
+++ lib.js
@@ -0,0 +1,13 @@
+let baseUrl = "/example";
+let routeConfig = [
+    {
+        "title":"vue基础",
+        "menu":[
+            {
+                "name":"v-if和v-html",
+                "url":baseUrl+"/1/1.html"
+            },
+        ]
+    },
+]
+export{routeConfig}
